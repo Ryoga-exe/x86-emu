cde212d084d8a1fee49947d6b9e93ced467c5b52
Ryoga-exe <git@ryoga.dev>
2024-05-01 21:13:14 +0900

    feat: add simple io and emulator

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,71 @@
 const std = @import("std");
+const MemorySize = 1024 * 1024; // 1MB
+const Allocator = std.mem.Allocator;
+
+const Register = enum {
+    eax,
+    ecx,
+    edx,
+    ebx,
+    esp,
+    ebp,
+    esi,
+    edi,
+    size,
+};
+
+const Emulator = struct {
+    const Self = @This();
+
+    registers: [@intFromEnum(Register.size)]u32,
+    eflags: u32,
+    memory: []u8,
+    eip: u32,
+    allocator: Allocator,
+
+    pub fn init(allocator: Allocator, size: usize, eip: u32, esp: u32) !Self {
+        var self = Self{
+            .registers = [1]u32{0} ** @intFromEnum(Register.size),
+            .eflags = 0,
+            .memory = try allocator.alloc(u8, size),
+            .eip = eip,
+            .allocator = allocator,
+        };
+        self.registers[@intFromEnum(Register.esp)] = esp;
+        return self;
+    }
+    pub fn deinit(self: *Self) void {
+        self.allocator.free(self.memory);
+    }
+};
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const allocator = std.heap.page_allocator;
+    const args = std.process.argsAlloc(allocator) catch {
+        std.debug.print("out of memory\n", .{});
+        std.os.exit(1);
+    };
+    defer std.process.argsFree(allocator, args);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    if (args.len < 2) {
+        try stdout.print("usage: x86-emu filename\n", .{});
+        try bw.flush();
+        std.os.exit(1);
+    }
 
-    try bw.flush(); // don't forget to flush!
-}
+    var emu = try Emulator.init(allocator, MemorySize, 0x0000, 0x7c00);
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    var file = std.fs.cwd().openFile(args[1], .{}) catch {
+        std.debug.print("no such file: {s}\n", .{args[1]});
+        std.os.exit(1);
+    };
+    defer file.close();
+
+    var buffered = std.io.bufferedReader(file.reader());
+    var reader = buffered.reader();
+    _ = try reader.readAll(emu.memory);
 }
